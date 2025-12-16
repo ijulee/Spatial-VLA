ed53cd2951a1fa776f2e4f8f7444f095e27aaec2
Randy Bui <randybui@berkeley.edu>
2025-12-15 16:21:37 -0800

    started work on FSM conditions

--- control-stack/client.py
+++ control-stack/client.py
@@ -13,13 +13,13 @@ import time
 
 
 SERVER_URL = "https://ik92uwhwu2vm2v-8000.proxy.runpod.net"
-
+SERVER_URL_ALT = "https://ckse5dhl9e73el-8000.proxy.runpod.net/"
 img_path_test = "C:/Users/randy/Desktop/Spatial-VLA/Photos/scene_001_bus_06.png"
 import cv2 
 from ultralytics import YOLO
 
 
-def send_to_VLM(img) :
+def send_to_VLM(img,phase) :
     # img = cv2.imread(img_path_test,cv2.IMREAD_COLOR)
     if img is not None:
         sucess,buffer = cv2.imencode('.jpg',img)
@@ -28,24 +28,24 @@ def send_to_VLM(img) :
             img_bytes = buffer.tobytes()
             img_b64 = base64.b64encode(img_bytes).decode('utf-8')
             # try:
-
+            prompt =   "Give me the coordinates of the closest man in the type of a python Dict (ONLY GIVE ME THE DICT) {'x': x_val, 'y': y_val}, if you cant give me {'x': x_val, 'y': y_val} NEVER explain yourself, only the dict" if (phase=="People")  else "Give me the coordinates of the closest Bench in the type of a python Dict (ONLY GIVE ME THE DICT) {'x': x_val, 'y': y_val}, if you cant give me {'x': x_val, 'y': y_val} NEVER explain yourself, only the dict" 
             payload = {
                 "image": img_b64,
-                "prompt": "in this format and only this format (NO TEXT AT ALL) {'X' : x_val, 'Y' : y_val} give me the coordinates of the bench closest to the man on the top right",
-                "max_tokens": 1000000, #MESS AROUND WITH THIS
+                "prompt": prompt,
+                "max_tokens": 100000, #MESS AROUND WITH THIS
                 "temperature": 1.0
             }
             # print(payload["image"])
             start = time.perf_counter()
             response = requests.post(
-                f"{SERVER_URL}/inference",
+                f"{SERVER_URL_ALT}/inference",
                 json=payload
                 # timeout=30
             )
             timing = time.perf_counter() - start
             print(response.json())
             print("RESPONSE TIME: " , timing, " seconds" )
-        return response.json()
+        return response
     return None
 
 # def MetaData(results):
@@ -54,6 +54,12 @@ def send_to_VLM(img) :
 #         boxes = result.boxes
 #         for box in boxes:
 #             print(box.cid)
+def display(img) -> None:
+    cv2.imshow("CAMERA VIEW", img) #Just display for live demo purposes i guess
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+    
+
 
 def get_clock_box( results):
     """
@@ -70,7 +76,7 @@ def get_clock_box( results):
             class_id = int(box.cls[0].cpu().numpy())
             class_name = result.names[class_id]
             
-            if class_name == "clock":
+            if class_name == "person":
                 # Get coordinates in different formats
                 xyxy = box.xyxy[0].cpu().numpy()  # [x1, y1, x2, y2]
                 xywh = box.xywh[0].cpu().numpy()  # [center_x, center_y, width, height]
@@ -88,16 +94,70 @@ def get_clock_box( results):
     
     return None
 
-def getCoords(response):
-    coords = response["text"]
-    return [coords["x"], coords["y"]]
+
+def getPhases(latch, results):
+    try:
+        for result in results:
+            boxes = result.boxes
+            # Find clock in detections
+            for _,box in enumerate(boxes):
+                class_id = int(box.cls[0].cpu().numpy())
+                class_name = result.names[class_id]
+        if latch[0]:
+            #We are going to stay in the drop off phase, we will probably have to manually go to shut off or something
+            return "DropOff"
+        if "person" in class_name:
+            #Still have people on board, either travel or path find to them
+            return "People"
+        if "person" not in class_name:
+            #No longer in people finding phase,  now in drop off phase
+            latch.append(True)
+            latch.pop(0)
+            return "DropOff"
+    except:
+        print("no detections")
+     
+
+#Get new coords depending on distance VLM tells us to go
+def getCommand(results,xy):
+    clock_meta_data = get_clock_box(results)
+    if(clock_meta_data['xywh'][0] < xy[0]):
+        return 'D'
+    if(clock_meta_data['xywh'][0] > xy[0]):
+        return 'B'
+    if(clock_meta_data['xywh'][0] > xy[1]):
+        return 'L'
+    if(clock_meta_data['xywh'][0] > xy[1]):
+        return 'R'
+    
+
+
+
+def getCoords(results,response): 
+    # return [coords["x"], coords["y"]]
+    coords = {"x":0,"y": 0}
+
+    clock_meta_data = get_clock_box(results)
+    try:
+        coords = response["text"]
+
+        coords = [coords["x"]+clock_meta_data['xywh'][0],coords["y"]+clock_meta_data['xywh'][1]]
+    except:
+        print("missed")
+    finally:
+        return coords
 
 def getNewCriticalPoint(results,coords):
-    clock_meta_data = get_clock_box(results)
-    print(clock_meta_data)
-    if(coords[0] < clock_meta_data['xyxy'][0] or coords[0] > clock_meta_data['xyxy'][2] or coords[1] > clock_meta_data['xyxy'][1] or coords[1] > clock_meta_data['xyxy'][3]):
-        return True
-    return False
+    try:
+        clock_meta_data = get_clock_box(results)
+        print(clock_meta_data)
+        if clock_meta_data is not None:
+            if((coords[0] > clock_meta_data['xyxy'][0] and coords[0] < clock_meta_data['xyxy'][2]) and (coords[1] > clock_meta_data['xyxy'][1] and coords[1] < clock_meta_data['xyxy'][3])):
+                return True
+    except:
+        return False
+        
+
 
 #TEST BENCH CODE:
 # img = cv2.imread(img_path_test)
@@ -130,26 +190,49 @@ camera_id = 0
 camera = cv2.VideoCapture(camera_id)
 model = YOLO('yolov8n.pt')
 first_instance=True
+xy = [250,250]
+latch = [False]
+
 while(True): 
     ret,img = camera.read()
     if not ret:
         print("MISSING CAPTURE: NOT BREAKING -- HOPEFULLY WE RECAPTURE")
     else:
+
         results = model(
         source=img, #The camera port
-        device=0,  #Cuda device (gpu)
+        device="cpu",  #Cuda device (gpu)
         verbose=False #shuts it up
         )
 
-        boxed_img =  cv2.cvtColor(results[0].plot(),cv2.COLOR_RGB2BGR)
+        boxed_img =  results[0].plot()
+        display(boxed_img)
 
-        cv2.imshow("CAMERA VIEW", boxed_img) #Just display for live demo purposes i guess
-        
-        if(first_instance or getNewCriticalPoint(xy,boxed_img)):
-            response = send_to_VLM(img)
-            xy = getCoords(response)
+
+
+        if(first_instance or getNewCriticalPoint(results,xy)):
+            response = send_to_VLM(img,getPhases(latch,results))
+            xy = getCoords(results,response.json())
             first_instance = False
-        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # print(latch,getPhases(latch,results))
+        # match(getCommand(results,xy)):
+        #     case 'D':
+        #         #Drive forward command
+        #     case 'B':
+        #         #Reverse
+        #     case 'R':
+        #         #turn 90 degrees drive forward
+        #     case 'L':
+        #         #turn -90 degrees drive forward
+        #     case _:
+        #         #reorient with whatever direction we started with.
+                 
+
+            
+cv2.destroyAllWindows()
 
 
 
